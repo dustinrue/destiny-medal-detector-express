e11e6e240116393ccccd74bfad14d7c2c24559dc
dustinrue <ruedu@dustinrue.com>
2016-11-17 16:59:14 -0600

    downloads URLs based on gt and clipid

--- index.js
+++ index.js
@@ -2,39 +2,48 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var medalDetector = require('destiny-medal-detector');
 var request = require('request');
+var xboxLive = require('xbox-live-api');
 var fs = require('fs');
 var tmp = require('tmp');
 var URL = require('url');
 var app = express();
+
+xboxLive.username = process.env.XBLUSERNAME;
+xboxLive.password = process.env.XBLPASSWORD
+xboxLive.useragent = "Destiny Medal Detector"; 
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 app.post('/detectmedals', function(req, res) {
   var medals = req.body.medals
-  var url = req.body.url;
-  var parsedUrl = URL.parse(url);
+  var gt = req.body.gt
+  var vid = req.body.vid;
   
-  if (!parsedUrl.hostname.match(/gameclipscontent.+xboxlive.com/i)) {
-    res.send("invalid clip url");
-  }
-  else {
+
+  xboxLive.GetDetailsForClip(gt, '247546985', vid, function(json) {
     var tmpVideoFile = tmp.fileSync();
     var file = fs.createWriteStream(tmpVideoFile.name);
-    request(url).pipe(file)
+    var url = json.gameClipUris[0].uri;
+    console.log("Downloading " + vid + " for " + gt);
+    request(url).pipe(file);
 
     file.on('finish', function() {
-      console.log("Processing " + tmpVideoFile.name);
+      console.log("Processing " + vid + " for " + gt);
       medalDetector.detectMedals(tmpVideoFile.name, medals, function(medalsDetected) {
         res.send(medalsDetected);
-        fs.unlink(tmpVideoFile.name);
+        console.log("Detected " + medalsDetected.join(", "));
+        fs.unlink(tmpVideoFile.name, function() {
+          
+        });
       });
     });
-  }
-    
+  
+  });
 });
 
+
 app.listen(3000, function() {
-  console.log("Ready");
+  console.log("Destiny Medal Detector is ready");
   
 });
